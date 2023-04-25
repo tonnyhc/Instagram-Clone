1bb5ddd7e7f6610dad7dce6b06413fb73a3b89e3
Toni <smokercho56@gmail.com>
2023-04-25 20:58:54 +0300

    Adding new server

---
+++ server/server/accounts/models.py
@@ -0,0 +1,60 @@
+from django.db import models
+
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin, Group, Permission
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
+class AppUser(AbstractBaseUser, PermissionsMixin):
+    MAX_LEN_USERNAME = 30
+    MAX_LEN_FULL_NAME = 150
+
+    email = models.EmailField(unique=True)
+    username = models.CharField(
+        max_length=MAX_LEN_USERNAME,
+        unique=True
+    )
+    full_name = models.CharField(
+        max_length=MAX_LEN_FULL_NAME
+    )
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    groups = models.ManyToManyField(
+        Group,
+        verbose_name='groups',
+        blank=True,
+        related_name='appuser_set',  # add related_name argument
+        help_text=
+            'The groups this user belongs to. A user will get all permissions '
+            'granted to each of their groups.',
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        verbose_name='user permissions',
+        blank=True,
+        related_name='appuser_set',  # add related_name argument
+        help_text='Specific permissions for this user.',
+    )
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
