fc54658f16f062419485d3adb9b77360d45816b7
Toni <smokercho56@gmail.com>
2023-07-02 16:04:08 +0300

    Created the post modal

--- server/server/posts/models.py
+++ server/server/posts/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+from server.profiles.models import Profile
+
+
+class Post(models.Model):
+    MAX_LEN_CAPTION = 500
+    MAX_LEN_LOCATION = 80
+    creator = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE
+    )
+    caption = models.CharField(
+        max_length=MAX_LEN_CAPTION,
+        blank=True,
+        null=True
+    )
+    location = models.CharField(
+        max_length=MAX_LEN_LOCATION,
+        blank=True,
+        null=True,
+    )
+    disabled_comments = models.BooleanField(
+        default=False,
+
+    )
+    hidden_likes = models.BooleanField(
+        default=False
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
